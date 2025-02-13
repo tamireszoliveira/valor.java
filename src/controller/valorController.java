5536b0befcd79bc4cbd5307d80790ce839044417
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-13 15:27:52 -0300

    mudança enunciado

--- src/controller/valorController.java
+++ src/controller/valorController.java
@@ -1,11 +1,7 @@
 package controller;
 
 public class valorController {
-	/* 2. Criar uma aplicação em Java que tenha uma função recursiva que, recebendo um vetor de inteiros, o
-	tamanho do vetor e o valor da úl�ma posição do vetor como o primeiro menor valor, retorne o menor
-	valor con�do neste vetor.
-	O Código deve apresentar, em formato de comentário, como foi definida a condição de parada;
-	O Código deve apresentar, em formato de comentário, como foi definida a relação de chamada dos passos; */
+	
 	
 	public valorController() {
 		super();
--- src/view/main.java
+++ src/view/main.java
@@ -1,7 +1,11 @@
 package view;
 
 import controller.valorController;
-
+	/* 2. Criar uma aplicação em Java que tenha uma função recursiva que, recebendo um vetor de inteiros, o
+	tamanho do vetor e o valor da úl�ma posição do vetor como o primeiro menor valor, retorne o menor
+	valor con�do neste vetor.
+	O Código deve apresentar, em formato de comentário, como foi definida a condição de parada;
+	O Código deve apresentar, em formato de comentário, como foi definida a relação de chamada dos passos; */
 public class main {
 	public static void main(String [] args) {
 		int [] vetor = {50, 89, 3, 7, 5};
